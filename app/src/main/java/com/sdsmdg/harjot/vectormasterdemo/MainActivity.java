0655b537aa4e89f758df8b6841aa2da268f91f0f
harjot-oberai <harjot.oberai@gmail.com>
2017-06-23 22:54:46 +0530

    Updated cover gif

--- app/src/main/java/com/sdsmdg/harjot/vectormasterdemo/MainActivity.java
+++ app/src/main/java/com/sdsmdg/harjot/vectormasterdemo/MainActivity.java
@@ -214,8 +214,7 @@ public class MainActivity extends AppCompatActivity {
                 }
 
             }
-        }, 200, 1000 / 60);
-
+        }, 1000, 1000 / 60);
     }
 
     void animateRain() {
